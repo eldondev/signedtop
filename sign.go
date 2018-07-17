bd0f7708b7f8049b3fe0ad37b6d691d8938b1e4f
Eldon Stegall <eldon@eldondev.com>
2018-07-17 05:32:21 +0000

    Working signing with tests

--- sign.go
+++ sign.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"crypto"
 	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/asn1"
 	"encoding/base64"
+	"encoding/json"
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"math/big"
 	"os"
 )
+
+var GENERATED_KEY_NAME = ".eldon-generated-key"
 
 type SignedMessage struct {
 	Message   string
@@ -20,8 +27,16 @@ type SignedMessage struct {
 	PubKey    string
 }
 
-func (c *SignedMessage) generateSignature() error {
-	return errors.New("Not yet implemented!")
+func (c *SignedMessage) generateSignature(key *ecdsa.PrivateKey, rand io.Reader) error {
+	var hash [32]byte = sha256.Sum256([]byte(c.Message))
+	bytes, err := key.Sign(rand, hash[:], crypto.SHA256)
+	if err != nil {
+		return err
+	}
+	c.Signature = base64.StdEncoding.EncodeToString(bytes)
+	marshalled_pubkey, err := x509.MarshalPKIXPublicKey(key.Public())
+	c.PubKey = string(pem.EncodeToMemory(&pem.Block{Type: "PUBLIC KEY", Bytes: marshalled_pubkey}))
+	return err
 }
 
 type ecdsaSig struct {
@@ -42,7 +57,6 @@ func (s *SignedMessage) VerifySignedMessage() error {
 			ecdsaKey, ok := key.(*ecdsa.PublicKey)
 			if ok {
 				return s.verifyECDSA(ecdsaKey, raw_sig)
-
 			} else {
 				return fmt.Errorf("Unknown key type!")
 			}
@@ -66,7 +80,78 @@ func (s *SignedMessage) verifyECDSA(key *ecdsa.PublicKey, raw_sig []byte) error 
 	return nil
 }
 
-func main() {
+func loadKey(pem_bytes []byte) (private_key *ecdsa.PrivateKey, err error) {
+	var pem_block *pem.Block
+	var rest []byte
+	pem_block, rest = pem.Decode(pem_bytes)
+	if len(rest) != 0 || pem_block == nil || pem_block.Type != "PRIVATE KEY" {
+		return nil, fmt.Errorf("Improperly generated key file")
+	}
+	if key, err := x509.ParsePKCS8PrivateKey(pem_block.Bytes); err != nil {
+		return nil, err
+	} else {
+		if key, ok := key.(*ecdsa.PrivateKey); ok {
+			return key, nil
+		} else {
+			return nil, errors.New("Error casting key to ecdsa Private key")
+		}
+	}
+}
 
-	log.Printf("%+v", (&SignedMessage{Message: os.Args[1]}).generateSignature())
+func persistKey(private_key *ecdsa.PrivateKey) (private_key_out *ecdsa.PrivateKey, err error) {
+	marshalled_key, err := x509.MarshalPKCS8PrivateKey(private_key)
+	if err != nil {
+		return nil, errors.New("Cannot marshal key to bytes")
+	}
+	pem_encoded_key := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: marshalled_key})
+	err = ioutil.WriteFile(GENERATED_KEY_NAME, pem_encoded_key, 0600)
+	if err != nil {
+		return nil, err
+	}
+	return private_key, nil
+}
+
+func loadOrGenerateKey() (private_key *ecdsa.PrivateKey, rand io.ReadCloser, err error) {
+	rand, err = os.Open("/dev/urandom")
+	if err != nil {
+		return nil, nil, errors.New("Cannot open entropy device /dev/urandom")
+	}
+	var pem_bytes []byte
+	if pem_bytes, err = ioutil.ReadFile(GENERATED_KEY_NAME); err == nil {
+		key, err := loadKey(pem_bytes)
+		return key, rand, err
+	} else {
+		var new_key *ecdsa.PrivateKey
+		new_key, err = ecdsa.GenerateKey(elliptic.P384(), rand)
+		if err != nil {
+			return nil, nil, errors.New("Cannot generate new key")
+		}
+		key, err := persistKey(new_key)
+		return key, rand, err
+	}
+}
+
+func main() {
+	key, rand, err := loadOrGenerateKey()
+	defer rand.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(os.Args) == 1 {
+		log.Fatal("No message to be signed passed")
+	}
+	tosign := &SignedMessage{Message: os.Args[1]}
+	err = tosign.generateSignature(key, rand)
+	if err != nil {
+		log.Fatal(err)
+	}
+	output, err := json.Marshal(tosign)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tosign.VerifySignedMessage()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(string(output))
 }
--- sign_test.go
+++ sign_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-//	"crypto/ecdsa"
-//	"crypto/sha256"
+	"bytes"
+	//	"crypto/ecdsa"
+	//	"crypto/sha256"
 	"encoding/json"
+	"io/ioutil"
 	"os"
-//	"strings"
+	//	"strings"
 	"testing"
 )
 
@@ -16,15 +18,40 @@ func TestKnownGood(t *testing.T) {
 	var file *os.File
 	var err error
 	if file, err = os.Open("known_good.json"); err != nil {
-		t.Errorf("Could not open known good file: %s", err)
+		t.Errorf("Could not open known_good file: %s", err)
 	}
 	decoder := json.NewDecoder(file)
 	var good_message SignedMessage
 	if err := decoder.Decode(&good_message); err != nil {
-		t.Errorf("Could not read known good file: %s", err)
+		t.Errorf("Could not decode json from known_good file: %s", err)
 	}
-	err = good_message.VerifySignedMessage();
+	err = good_message.VerifySignedMessage()
 	if err != nil {
-		t.Error(err);
-		}
+		t.Error(err)
+	}
+}
+
+func TestMainRaw(t *testing.T) {
+	os.Args = []string{"bump", "thump"};
+	main()
+}
+
+func TestMainPerservePkey(t *testing.T) {
+	tmp, tmp_err := ioutil.TempFile("", "key")
+	os.Args = []string{"bump", "thump"};
+	os.Remove(tmp.Name())
+	GENERATED_KEY_NAME = tmp.Name()
+	main()
+	key, err := ioutil.ReadFile(GENERATED_KEY_NAME)
+	if len(key) == 0 || err != nil || tmp_err != nil {
+		t.Fatalf("No key generated")
+	}
+	main()
+	key2, err := ioutil.ReadFile(GENERATED_KEY_NAME)
+	if len(key2) == 0 || err != nil || tmp_err != nil {
+		t.Fatalf("No key generated")
+	}
+	if bytes.Compare(key, key2) != 0 {
+		t.Fatalf("Key not reused:\n %+v,\n %+v", key, key2)
+	}
 }
